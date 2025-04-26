ce9539e742ac700eafd05fe5dbd3fb6810330ad7
ayanshuDS <73418468+MET4L-DS@users.noreply.github.com>
2025-04-26 16:54:52 +0530

    Revert "ShadCn test"
    This reverts commit 32972102ecfd10241079bfd7c636ff781c2f8df4.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,37 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 import { Button } from "@/components/ui/button";
-import { MedicineTable } from "@/components/ui/MedicineTable";
 
 function App() {
+	const [count, setCount] = useState(0);
+
 	return (
-		<div className="flex flex-col items-center justify-center min-h-screen h-full">
-			<div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-				<Button>Click me</Button>
-				<div className="mt-8 w-full">
-					<MedicineTable />
-				</div>
+		<>
+			<div className=" flex justify-center">
+				<a href="https://vite.dev" target="_blank">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://react.dev" target="_blank">
+					<img
+						src={reactLogo}
+						className="logo react"
+						alt="React logo"
+					/>
+				</a>
 			</div>
-		</div>
+			<h1>Vite + React</h1>
+			<div className="card">
+				<Button variant="default">ShadCn</Button>
+				<p>
+					Edit <code>src/App.jsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className="read-the-docs">
+				Click on the Vite and React logos to learn more
+			</p>
+		</>
 	);
 }
 
