6297b27a8714fcef48ede33b3e4e928218dcaa4b
Harshit <hratusaria@gmail.com>
2025-04-29 09:06:29 +0530

    modified:   medical_shop/api/__pycache__/urls.cpython-313.pyc 	modified:   medical_shop/api/__pycache__/views.cpython-313.pyc 	modified:   medical_shop/api/urls.py 	modified:   medical_shop/api/views.py

--- medical_shop/api/urls.py
+++ medical_shop/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
+from . import views
 from .views import AddMedicineView
+from .views import get_all_products
 urlpatterns = [
     
     path('add-medicine/',AddMedicineView.as_view(),name='add-medicine'),
+    path('products/',views.get_all_products,name='get_all_products'),
 ]
--- medical_shop/api/views.py
+++ medical_shop/api/views.py
@@ -3,10 +3,10 @@ from rest_framework.views import APIView
 # Create your views here.from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
-from django.db import connection
+from django.db import connection,DatabaseError
 from django.utils.decorators import method_decorator
 @method_decorator(csrf_exempt, name='dispatch')
 class AddMedicineView(APIView):
@@ -49,5 +49,38 @@ class AddMedicineView(APIView):
         except Exception as e:
             print("Error:", str(e))
             return Response({'error': 'Something went wrong while saving the medicine.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+@csrf_exempt
+def get_all_products(request):
+    if request.method == "GET":
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute("""
+                    SELECT 
+                        *
+                    FROM product
+                """)
+                columns = [col[0] for col in cursor.description]
+                results = [
+                    dict(zip(columns, row))
+                    for row in cursor.fetchall()
+                ]
+            return JsonResponse(results, safe=False, status=200)
 
+        except DatabaseError as e:
+            # Database-related errors
+            return JsonResponse(
+                {"error": "Database error occurred", "details": str(e)},
+                status=500
+            )
+        except Exception as e:
+            # Any other unexpected errors
+            return JsonResponse(
+                {"error": "An unexpected error occurred", "details": str(e)},
+                status=500
+            )
 
+    else:
+        return JsonResponse(
+            {"error": "Method not allowed. Use GET."},
+            status=405
+        )
